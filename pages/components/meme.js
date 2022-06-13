1e1f010d8550842816f4c9a274f86e4f16ba5ed1
Nadia Iqbal <nadiaiqbal2600@example.com>
2022-06-13 12:36:18 +0500

    Added memeData.js(usestate #onclick )

--- pages/components/meme.js
+++ pages/components/meme.js
@@ -1,11 +1,41 @@
 import "bootstrap/dist/css/bootstrap.css";
+import memesData from "../memeData.js";
+import React from "react";
 
 export default function Meme() {
+  // let url
+  //   function getMemeImage() {
+  //     const memesArray = memesData.data.memes;
+  //     const randomNumber = Math.floor(Math.random() * memesArray.length);
+  //     const url = memesArray[randomNumber].url;
+  //   }
+
+  // challange: save the random meme url in state
+  // create new state called  `memeImage` with an empty string as default
+  // when the `getMemeImage`function is called , update the `memeImage `state to the random chosen image url
+  // below the div.form , add an <img/> and set the src to the new `memeImage` state we create
+
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
   return (
-    <form className="forrm main">
-      <input type="text" placeholder="top text" className="form--input" />
-      <input type="text" placeholder="bottom text" className="form--input" />
-      <button className="form--button">Get a new meme image </button>
-    </form>
+    <div>
+      {/* <p>{url}</p> */}
+      <form className="forrm main">
+        <input type="text" placeholder="top text" className="form--input" />
+        <input type="text" placeholder="bottom text" className="form--input" />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image
+        </button>
+      </form>
+      <img src={memeImage} className="meme--image" />
+    </div>
   );
 }
+// get a random image from `memesData` . array
+// when the new meme image button is clicked
